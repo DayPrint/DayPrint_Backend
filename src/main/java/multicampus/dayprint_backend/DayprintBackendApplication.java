f86ab5aa99ae9ad2a5087a5eab0f8a9b3fefd6c8
burigori <jamasa97@icloud.com>
2022-05-09 10:29:20 +0900

    init

---
+++ src/main/java/multicampus/dayprint_backend/DayprintBackendApplication.java
@@ -0,0 +1,13 @@
+package multicampus.dayprint_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayprintBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DayprintBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/multicampus/dayprint_backend/DayprintBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package multicampus.dayprint_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayprintBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
